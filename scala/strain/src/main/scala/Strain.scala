43a4448edf7fdef245254fa2b41c3beed15fac5b
mzorro <mzorrox@gmail.com>
2017-03-20 22:51:25 +0800

    Strain improve.

--- scala/strain/src/main/scala/Strain.scala
+++ scala/strain/src/main/scala/Strain.scala
@@ -1,11 +1,6 @@
 object Strain {
   def keep[T](input: Seq[T], predicate: (T) => Boolean): Seq[T] =
-    if (input.isEmpty)
-      input
-    else if (predicate(input.head))
-      Seq(input.head) ++ keep(input.tail, predicate)
-    else
-      keep(input.tail, predicate)
+    input.foldLeft(Seq.empty[T])((seq, t) => if (predicate(t)) seq :+ t else seq)
 
   def discard[T](input: Seq[T], predicate: (T) => Boolean): Seq[T] =
     keep(input, (t: T) => !predicate(t))
