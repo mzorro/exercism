8bcfb9124bb7b9ef8a1a45a5a986e1c4263a6f71
mzorro <mzorrox@gmail.com>
2017-03-21 22:46:56 +0800

    strain

--- scala/strain/src/main/scala/Strain.scala
+++ scala/strain/src/main/scala/Strain.scala
@@ -1,6 +1,6 @@
 object Strain {
   def keep[T](input: Seq[T], predicate: (T) => Boolean): Seq[T] =
-    input.foldRight(List.empty[T])((t, seq) => if (predicate(t)) t +: seq else seq)
+    input.foldRight(List.empty[T])((t, seq) => if (predicate(t)) t :: seq else seq)
 
   def discard[T](input: Seq[T], predicate: (T) => Boolean): Seq[T] =
     keep(input, !predicate(_: T))
