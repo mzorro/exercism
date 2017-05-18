762f2857033ee4495ec22ae53b559a9a0150c0e1
彭君健 <hzpengjunjian@corp.netease.com>
2017-05-18 11:38:06 +0800

    Robot simulator

---
+++ scala/robot-simulator/src/main/scala/Robot.scala
@@ -0,0 +1,55 @@
+/**
+  * Created On 05/18 2017
+  *
+  * @author hzpengjunjian@corp.netease.com
+  */
+object Bearing extends Enumeration {
+  type Bearing = Value
+  val North, East, South, West = Value
+
+  def step(bearing: Bearing): (Int, Int) = bearing match {
+    case North => (0, 1)
+    case East => (1, 0)
+    case South => (0, -1)
+    case West => (-1, 0)
+  }
+
+  def left(bearing: Bearing): Bearing = bearing match {
+    case North => West
+    case East => North
+    case South => East
+    case West => South
+  }
+
+  def right(bearing: Bearing): Bearing = bearing match {
+    case North => East
+    case East => South
+    case South => West
+    case West => North
+  }
+}
+
+import Bearing._
+case class Robot(bearing: Bearing, coordinates: (Int, Int)) {
+  val (x, y) = coordinates
+
+  implicit class Tuple2WithAddition(pair1: (Int, Int)) {
+    def +(pair2: (Int, Int)): (Int, Int) = {
+      (pair1._1 + pair2._1, pair1._2 + pair2._2)
+    }
+  }
+
+  def advance() = Robot(bearing, coordinates + step(bearing))
+
+  def turnLeft() = Robot(left(bearing), coordinates)
+
+  def turnRight() = Robot(right(bearing), coordinates)
+
+  def simulate(instructions: String): Robot =
+    if (instructions.isEmpty) this
+    else instructions.head match {
+      case 'L' => turnLeft simulate instructions.tail
+      case 'R' => turnRight simulate instructions.tail
+      case 'A' => advance simulate instructions.tail
+    }
+}
